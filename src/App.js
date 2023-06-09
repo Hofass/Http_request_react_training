c7b31816d87f865bdf5cbd37feda3098fc397b16
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-09 23:40:16 +0200

    Handling errors

--- src/App.js
+++ src/App.js
@@ -5,24 +5,29 @@ import "./App.css";
 
 function App() {
 	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false)
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	async function fetchMoviesHandler() {
 		setIsLoading(true);
-		const response = await fetch("https://swapi.dev/api/films/");
-		const data = await response.json();
+		setError(null);
+	try {
+	const response = await fetch("https://swapi.dev/api/films/");
+	const data = await response.json();
+	const transformedMovies = data.results.map(movieData => {
+		return {
+			id: movieData.episode_id,
+			title: movieData.title,
+			openingText: movieData.opening_crawl,
+			releaseData: movieData.release_date,
+		};
+	});
+	setMovies(transformedMovies);
+	setIsLoading(false)
+} catch{
 
-		const transformedMovies = data.results.map(movieData => {
-			return {
-				id: movieData.episode_id,
-				title: movieData.title,
-				openingText: movieData.opening_crawl,
-				releaseData: movieData.release_date,
-			};
-		});
-		setMovies(transformedMovies);
-		setIsLoading(false)
-	}
+}
+}
 	return (
 		<React.Fragment>
 			<section>
